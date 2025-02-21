cf7d2494cec496b72ca868c98f92deb221beef31
gilduck23 <gilduck23@gmail.com>
2025-02-21 21:13:52 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.html", "./*.js"],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': ['"Nunito Sans"', 'sans-serif'],
+    },
+    letterSpacing: {
+     tightest: '-.075em',
+    tighter: '-.05em',
+    tight: '-.025em',
+    normal: '0',
+    wide: '.025em',
+    wider: '.05em',
+    widest: '.25em',
+   },
     extend: {},
   },
   plugins: [],
 }
-
